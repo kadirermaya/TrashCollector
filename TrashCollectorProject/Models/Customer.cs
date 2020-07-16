7f87861afb824fd860e8316b119d62605a2d2a9e
kadirermaya <kadir.ermaya@gmail.com>
2020-07-16 14:16:25 -0700

    Models created for Customer and Employee

---
+++ TrashCollectorProject/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace TrashCollectorProject.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DayOfWeek PickUpDay { get; set; }
+        
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+    }
+}
---
+++ TrashCollectorProject/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollectorProject.Models
+{
+    
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "First Name is a Required Input")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public int ZipCode { get; set; }
+       
+        [ForeignKey("User")]
+        [Display(Name = "User Name")]
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+    }
+}
